365b55dfbb39c348b9e6f4ebc32f1d0e1d33e536
Utilisateur <user@debian.arcahe.ovh>
2022-02-11 10:17:47 +0100

    Ajout début fichier

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -1,1 +1,7 @@
-
+#####################################################
+# groupe LDDBI
+# Raphael DUPUY
+# 
+# 
+# https://github.com/raphaelDupuy/projet_tas_de_sable
+#####################################################
