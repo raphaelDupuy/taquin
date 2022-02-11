f93ff16aa880a72002d8cc643cc0f6a6ea10fb8f
Utilisateur <user@debian.arcahe.ovh>
2022-02-11 10:31:18 +0100

    Ajout découpage code

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -5,3 +5,22 @@
 # 
 # https://github.com/raphaelDupuy/projet_tas_de_sable
 #####################################################
+
+# Imports
+#########
+import tkinter as tk
+
+# Constantes
+############
+
+
+# Variables globales
+####################
+
+
+# Fonctions
+###########
+
+
+# Programme
+###########
