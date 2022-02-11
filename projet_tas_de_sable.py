88d5e460592f08db4b8a33b2cad0f6876616f797
Utilisateur <user@debian.arcahe.ovh>
2022-02-11 11:47:37 +0100

    ajout variable configuration_courrante et fonction init_random_config

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -8,6 +8,7 @@
 
 # imports librairies
 import tkinter as tk
+import random
 
 
 #####################################################
@@ -22,10 +23,20 @@ LARGEUR = 600
 #variables globales
 
 
+configuration_courrante = [[0, 0, 0]] * 3
+
 
 #####################################################
 
 #fonctions
+def init_config_courrante(config):
+    pass
+
+def init_random_config(config):
+    for i in config:
+        for j in i:
+            i[j] = random.randint(1, 3)
+    return config
 
 
 
