4e0e615bb046f995d9f313ed05a6e8550571e3c9
uvsq22103869 <91952616+uvsq22103869@users.noreply.github.com>
2022-03-04 19:11:37 +0100

    Update projet_tas_de_sable.py

--- projet_tas_de_sable.py
+++ projet_tas_de_sable.py
@@ -2,7 +2,7 @@
 # groupe LDDBI
 # Raphael DUPUY
 # Soumaya MEDIOUNI
-# 
+# Ebeguy Yapo
 # https://github.com/raphaelDupuy/projet_tas_de_sable
 #####################################################
 
