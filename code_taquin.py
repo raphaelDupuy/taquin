1c8c2e399e2658a1020e377d60bc2c3c6b6e0a1b
Utilisateur <user@debian.arcahe.ovh>
2022-04-15 11:05:33 +0200

    function random grille

--- code_taquin.py
+++ code_taquin.py
@@ -1,1 +1,26 @@
+# imports
+import random
 
+# globaux
+grille = [[0]*4, [0]*4, [0]*4, [0]*4]
+
+# fonctions
+
+
+def random_start():
+    global grille
+    cnt, var = 0, []
+    for i in range(0, 4):
+        for j in range(0, 4):
+            while cnt in var:
+                cnt = random.randint(0, 15)
+                if len(var) == 16:
+                    break
+            grille[i][j] = cnt
+            var.append(cnt)
+    a = grille[3][3]
+    grille[0][0], grille[3][3] = a, 0
+            
+
+random_start()
+print(grille)
