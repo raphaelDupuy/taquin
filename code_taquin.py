647feb8c0799894f30950117a14a8a253738926f
Utilisateur <user@debian.arcahe.ovh>
2022-04-15 11:41:56 +0200

    ajout fonctions move vers le haut

--- code_taquin.py
+++ code_taquin.py
@@ -18,9 +18,50 @@ def random_start():
                     break
             grille[i][j] = cnt
             var.append(cnt)
-    a = grille[3][3]
-    grille[0][0], grille[3][3] = a, 0
+    print(grille)
+    # a = grille[3][3]
+    # grille[0][0], grille[3][3] = a, 0
+
+
+def get_line():
+    global grille
+    if 0 in grille[0]:
+        ligne = 0
+    elif 0 in grille[1]:
+        ligne = 1
+    elif 0 in grille[2]:
+        ligne = 2
+    elif 0 in grille[3]:
+        ligne = 3
+    return ligne
+
+
+def move(direction):
+    global grille
+    # glisse une collone vers le haut
+    if direction == 'u':
+        ligne = get_line()
+        if ligne == 3:
+            pass
+        else:
+            colonne = grille[ligne].index(0)
+            for i in range(ligne, 3):
+                grille[i][colonne] = grille[i+1][colonne]
+            grille[3][colonne] = 0
+    
+
+
+def win():
+    pass
+
+
+def verif_victoire():
+    global grille
+    if grille == [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]]:
+        win()
+
             
 
 random_start()
+move('u')
 print(grille)
